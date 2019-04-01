418f62a6d1a9ec0dce36e94b93902820c3dabffc
Triston Armstrong <triston95strong@gmail.com>
2019-04-01 16:00:38 -0500

    converted for loops to forEach for practice

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -73,27 +73,23 @@ console.log(inventory[lastCar].car_make, inventory[lastCar].car_model);
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-for(let i = 0; i < inventory.length; i++){
-    carModels.push(inventory[i].car_model)
-}
+inventory.forEach(x => carModels.push(x.car_model))
 console.log(carModels.sort())
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-for(let i = 0; i < inventory.length; i++){
-    carYears.push(inventory[i].car_year)
-}
+inventory.forEach(x => carYears.push(x.car_year))
 console.log(carYears)
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
-for(let i=0; i<carYears.length; i++){
-    if(carYears[i] < 2000){
-        oldCars.push(carYears[i]);
+inventory.forEach(x => {
+    if(x.car_year<"2000"){
+        oldCars.push(x.car_year)
     }
-} 
+})
 console.log(oldCars)
 
 // ==== Challenge 6 ====
@@ -104,6 +100,11 @@ for(let i = 0; i<inventory.length; i++){
         BMWAndAudi.push(inventory[i]);
     }
 }
+inventory.forEach(x => {
+    if(x.car_make == "BMW" || x.car_make == "Audi"){
+        BMWAndAudi.push(x);
+    }
+})
 console.log(JSON.stringify(BMWAndAudi))
 
 
